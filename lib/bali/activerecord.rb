a09c2ea9e515d32b179f387610fc4719daccab54
Adam Pahlevi <adam.pahlevi@veritrans.co.id>
2015-08-26 17:49:05 +0700

    init

---
+++ lib/bali/activerecord.rb
@@ -0,0 +1,7 @@
+require "bali/activerecord/version"
+
+module Bali
+  module Activerecord
+    # Your code goes here...
+  end
+end
---
+++ lib/bali/activerecord/version.rb
@@ -0,0 +1,5 @@
+module Bali
+  module Activerecord
+    VERSION = "0.1.0"
+  end
+end
